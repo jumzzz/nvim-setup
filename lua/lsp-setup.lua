cdda20b9d703df75a5cc8542362dd5767b94c4ed
jumzzz <jummel19@gmail.com>
2024-07-15 11:57:51 +0800

    added zig

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -1,6 +1,14 @@
 local nvim_lsp = require('lspconfig')
 
 nvim_lsp.pyright.setup{}
-nvim_lsp.rust_analyzer.setup{}
+nvim_lsp.rust_analyzer.setup{
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+}
 nvim_lsp.clangd.setup{}
+
+-- For Zig
+nvim_lsp.zls.setup{}
 
